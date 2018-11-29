53d2cf9d7e4cec1057ec359b2919d5eedbe13117
jasonramo <jasonaramo@gmail.com>
2018-11-29 11:07:54 -0600

    Created a remove feature for the application

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,24 +1,22 @@
 package org.launchcode.cheesemvc.controllers;
 
 
+import org.launchcode.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    //static ArrayList<String> cheeses = new ArrayList<>();
-    static HashMap<String, String> cheeses = new HashMap<>();
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
-    // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
@@ -34,12 +32,32 @@ public class CheeseController {
         return "cheese/add";
     }
 
+
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName,
                                        @RequestParam String cheeseDescription) {
-        cheeses.put(cheeseName, cheeseDescription);
 
-        // Redirect to /cheese because
+        // instantiate a cheese object
+        Cheese cheese = new Cheese(cheeseName, cheeseDescription);
+        cheeses.add(cheese);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("description", cheeses);
+        model.addAttribute("title","Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+
+        for (int j : cheeseIds) {
+            cheeses.remove(j);
+        }
+
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,8 +1,38 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Cheese {
+
+    @NotNull
+    @Size(min=3, max=20)
     private String name;
+
+    @NotNull
+    @Size(min=1, message = "Description must not be empty")
     private String description;
+
+    private int cheeseId;
+    private static int nextId = 1;
+
+    public Cheese(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Cheese() {
+        cheeseId = nextId;
+        nextId++;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
 
     public String getName() {
         return name;
